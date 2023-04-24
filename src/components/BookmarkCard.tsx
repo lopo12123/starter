eeb4da4c9fd69f721124faab77b6cffef9f25f69
lopo <lopo@zju.edu.cn>
2023-04-24 21:46:02 +0800

    feat + style: 内边距 + 点击跳转

--- src/components/BookmarkCard.tsx
+++ src/components/BookmarkCard.tsx
@@ -1,19 +1,24 @@
-import {BookmarkItem} from "../scripts/storage";
+import { BookmarkItem } from "../scripts/storage";
 
 export type BookmarkCardProps = {
     item: BookmarkItem
 }
 
 export const BookmarkCard = (props: BookmarkCardProps) => {
-    const [link, date] = props.item
+    const [ link, date ] = props.item
 
     return (
-        <div className="bookmark-card">
-            <div className="link">
-                <b>内容:</b> {link}
+        <div className="bookmark-card"
+             onClick={ () => {
+                 if(link.startsWith('http')) {
+                     window.open(link)
+                 }
+             } }>
+            <div className="link" title={ link }>
+                <b>内容:</b> { link }
             </div>
             <div className="date">
-                <b>创建:</b> {date}
+                <b>创建:</b> { date }
             </div>
         </div>
     )
