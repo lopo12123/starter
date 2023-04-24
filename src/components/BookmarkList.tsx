0e6997d0d8a45c3eafda93dd320c09446f90b13a
lopo <lopo@zju.edu.cn>
2023-04-24 21:49:51 +0800

    feat + style: empty / not-empty

--- src/components/BookmarkList.tsx
+++ src/components/BookmarkList.tsx
@@ -1,7 +1,7 @@
-import {StorageImpl, SyncList, SyncMap} from "../scripts/storage";
+import { StorageImpl, SyncList, SyncMap } from "../scripts/storage";
 import Cat from "../assets/cat.gif";
-import {BookmarkCard} from "./BookmarkCard";
-import {useRecoilValue, useSetRecoilState} from "recoil";
+import { BookmarkCard } from "./BookmarkCard";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 
 export const BookmarkList = () => {
     const list = useRecoilValue(SyncList)
@@ -11,17 +11,17 @@ export const BookmarkList = () => {
         StorageImpl.removeSingle(id)
         setSyncMap(prev => {
             prev.delete(id)
-            return {...prev}
+            return { ...prev }
         })
     }
 
     return (
-        <div className="bookmark-list">
+        <div className={ `bookmark-list ${ list.length == 0 ? 'empty' : 'not-empty' }` }>
             {
                 list.length == 0
-                    ? <img src={Cat} alt="" style={{height: '80%', opacity: 0.7}}/>
+                    ? <img src={ Cat } alt="" style={ { height: '80%', opacity: 0.7 } }/>
                     : list.map(
-                        (item, idx) => <BookmarkCard key={idx} item={item}/>
+                        (item, idx) => <BookmarkCard key={ idx } item={ item }/>
                     )
             }
         </div>
