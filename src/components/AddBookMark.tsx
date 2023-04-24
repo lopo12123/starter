d7e38bfea5393a4fc0aaf2899d2dd94d19f420c3
lopo <lopo@zju.edu.cn>
2023-04-24 21:35:24 +0800

    chore: no unused

--- src/components/AddBookMark.tsx
+++ src/components/AddBookMark.tsx
@@ -1,15 +1,15 @@
-import {useState} from "react";
-import {toast} from "sonner";
-import {StorageImpl, SyncMap} from "../scripts/storage";
-import {useSetRecoilState} from "recoil";
+import { useState } from "react";
+import { toast } from "sonner";
+import { StorageImpl, SyncMap } from "../scripts/storage";
+import { useSetRecoilState } from "recoil";
 
 const AddBookMark = () => {
     const setSyncMap = useSetRecoilState(SyncMap)
-    const [url, setUrl] = useState('')
+    const [ url, setUrl ] = useState('')
     const addBookmark = () => {
-        if (!url) toast.error('请输入内容')
+        if(!url) toast.error('请输入内容')
         else {
-            const newItem = [url, new Date().toLocaleString()]
+            const newItem: [ string, string ] = [ url, new Date().toLocaleString() ]
             const newItemKey = StorageImpl.addSingle(newItem)
             setUrl('')
             setSyncMap(prev => {
@@ -24,12 +24,12 @@ const AddBookMark = () => {
     return (
         <div>
             <input
-                style={{width: 260}}
-                type="text" value={url}
+                style={ { width: 260 } }
+                type="text" value={ url }
                 placeholder="请输入"
-                onChange={e => setUrl(e.target.value)}/>
-            <button style={{marginLeft: 20}}
-                    onClick={addBookmark}>
+                onChange={ e => setUrl(e.target.value) }/>
+            <button style={ { marginLeft: 20 } }
+                    onClick={ addBookmark }>
                 添加
             </button>
         </div>
